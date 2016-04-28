c00c1195bf3c7467d1d826adf63e3f27758ed519
Wannes Gennar <wannes.gennar@gmail.com>
2016-04-28 23:33:56 +0200

    fix unit test

--- tests/Api/RegistrationTest.php
+++ tests/Api/RegistrationTest.php
@@ -6,14 +6,21 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class RegistrationTest extends TestCase
 {
+	use DatabaseMigrations;
+
 	public function testValidRegistration()
 	{
 		$user = [
-
+			'fname'                 => 'Wannes',
+			'lname'                 => 'Gennar',
+			'email'                 => 'foo@bar.com',
+			'password'              => 12345,
+			'password_confirmation' => 12345,
 		];
 
-		$this->post('/api/users/register', $user)
+		$this->post('/api/users/register', $user, [ 'Accept' => 'application/json' ])
 			->seeStatusCode(200)
-			->seeJson([ 'status' => 'OK' ]);
+			->seeJson()
+			->seeInDatabase('users', array_except($user, [ 'password', 'password_confirmation' ]));
 	}
 }
