bccdf57c5ac2ad424a0422f0069bc2509457bef8
Wannes Gennar <wannes.gennar@gmail.com>
2016-04-29 01:01:01 +0200

    added valid authentication test

--- tests/Api/AuthenticationTest.php
+++ tests/Api/AuthenticationTest.php
@@ -1,12 +1,23 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Festival\Entities\Users\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class AuthenticationTest extends TestCase
 {
+	use DatabaseMigrations;
+
 	public function testValidAuthentication()
 	{
+		$user = factory(User::class)->create([ 'password' => bcrypt(12345) ]);
+
+		$credentials = [
+			'email'    => $user->email,
+			'password' => 12345,
+		];
+
+		$this->postJson('/api/login', $credentials)
+			->seeStatusCode(200)
+			->seeJson([ 'token' => $user->secret ]);
 	}
 }
