109cfd5013765a91a4dcdfbfb75d1c599be00ac4
Wannes Gennar <wannes.gennar@gmail.com>
2016-04-28 17:48:32 +0200

    bind repositories in the container

--- src/Providers/AppServiceProvider.php
+++ src/Providers/AppServiceProvider.php
@@ -3,18 +3,23 @@
 namespace Festival\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Festival\Repositories\Users\EloquentUserRepository;
+use Festival\Contracts\Repositories\Users\UserRepository;
+use Festival\Repositories\Tickets\EloquentTicketRepository;
+use Festival\Repositories\Artists\EloquentArtistRepository;
+use Festival\Contracts\Repositories\Tickets\TicketRepository;
+use Festival\Contracts\Repositories\Artists\ArtistRepository;
+use Festival\Repositories\NewsItems\EloquentNewsItemRepository;
+use Festival\Contracts\Repositories\NewsItems\NewsItemRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+    protected $bindings = [
+		UserRepository::class => EloquentUserRepository::class,
+		ArtistRepository::class => EloquentArtistRepository::class,
+		TicketRepository::class => EloquentTicketRepository::class,
+		NewsItemRepository::class => EloquentNewsItemRepository::class,
+	];
 
     /**
      * Register any application services.
@@ -23,6 +28,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+		foreach ($this->bindings as $contract => $binding)
+			$this->app->bind($contract, $binding);
     }
 }
