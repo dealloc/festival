33b01f84c2096a981133e9178bbfb7ffc40bb0c3
Wannes Gennar <wannes.gennar@gmail.com>
2016-04-29 00:47:23 +0200

    added test class for authentication

---
+++ tests/Api/AuthenticationTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class AuthenticationTest extends TestCase
+{
+	public function testValidAuthentication()
+	{
+	}
+}
