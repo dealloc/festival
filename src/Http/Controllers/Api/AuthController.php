777d7a6ac47b042afe929c68d84a4480f10c574e
Wannes Gennar <wannes.gennar@gmail.com>
2016-04-29 00:41:55 +0200

    implemented user login

--- src/Http/Controllers/Api/AuthController.php
+++ src/Http/Controllers/Api/AuthController.php
@@ -6,7 +6,9 @@ namespace Festival\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use Festival\Http\Controllers\Controller;
 use Festival\Commands\Users\CreateUserCommand;
+use Festival\Contracts\Auth\AuthenticateService;
 use Festival\Http\Requests\Users\CreateUserRequest;
+use Festival\Exceptions\Auth\InvalidCredentialsException;
 
 class AuthController extends Controller
 {
@@ -15,7 +17,11 @@ class AuthController extends Controller
 		return $this->execute(new CreateUserCommand($request));
 	}
 
-	public function login(Request $request)
+	public function login(Request $request, AuthenticateService $service)
 	{
+		if ( ! $service->login($request->all()) )
+			throw new InvalidCredentialsException;
+
+		return [ 'token' => $service->user()->secret ];
 	}
 }
