ed32d54eb01c8fa5b2c3b4c4c7b7e26c46f5e70d
Wannes Gennar <wannes.gennar@gmail.com>
2016-04-29 00:53:18 +0200

    implemented bypass for laravel issue #13365

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,24 +2,55 @@
 
 class TestCase extends Illuminate\Foundation\Testing\TestCase
 {
-    /**
-     * The base URL to use while testing the application.
-     *
-     * @var string
-     */
-    protected $baseUrl = 'http://localhost';
+	const AJAX_HEADER = [ 'Accept' => 'application/json' ];
 
-    /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
-    public function createApplication()
-    {
-        $app = require __DIR__.'/../bootstrap/app.php';
+	/**
+	 * The base URL to use while testing the application.
+	 *
+	 * @var string
+	 */
+	protected $baseUrl = 'http://localhost';
 
-        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+	/**
+	 * Creates the application.
+	 *
+	 * @return \Illuminate\Foundation\Application
+	 */
+	public function createApplication()
+	{
+		$app = require __DIR__ . '/../bootstrap/app.php';
 
-        return $app;
-    }
+		$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+		return $app;
+	}
+
+	/**
+	 * Visit the given URI with a JSON GET request.
+	 *
+	 * @param  string  $uri
+	 * @param  array  $headers
+	 * @return $this
+	 */
+	public function getJson($uri, array $headers = [])
+	{
+		$headers['Accept'] = 'application/json';
+
+		return $this->get($uri, $headers);
+	}
+
+	/**
+	 * Visit the given URI with a POST request.
+	 *
+	 * @param  string  $uri
+	 * @param  array  $data
+	 * @param  array  $headers
+	 * @return $this
+	 */
+	public function postJson($uri, array $data = [ ], array $headers = [ ])
+	{
+		$headers['Accept'] = 'application/json';
+
+		return $this->post($uri, $data, $headers);
+	}
 }
