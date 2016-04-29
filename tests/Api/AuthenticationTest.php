b807fc230607cb8cc7c0c110cf667f9b93c02b59
Wannes Gennar <wannes.gennar@gmail.com>
2016-04-29 15:12:14 +0200

    add token regeneration in unit tests

--- tests/Api/AuthenticationTest.php
+++ tests/Api/AuthenticationTest.php
@@ -21,16 +21,34 @@ class AuthenticationTest extends TestCase
 			->seeJson([ 'token' => $user->secret ]);
 	}
 
+	public function testTokenRegenerationOnLogin()
+	{
+		$user = factory(User::class)->create([ 'password' => bcrypt(12345) ]);
+
+		$credentials = [
+			'email'    => $user->email,
+			'password' => 12345,
+		];
+
+		$this->postJson('/api/login', $credentials);
+
+		$token = json_decode($this->response->content())->token;
+
+		$this->postJson('/api/login', $credentials)
+			->seeStatusCode(200)
+			->dontSeeJson([ 'token' => $token ]);
+	}
+
 	public function testInvalidCredentials()
 	{
 		$this->postJson('/api/login', [ 'email' => 'invalid@invalid.invalid', 'password' => 'invalid' ])
 			->seeStatusCode(401)
-			->seeJson([	'error' => 'Unauthorized access' ]);
+			->seeJson([ 'error' => 'Unauthorized access' ]);
 	}
 
 	public function testEmptyEmail()
 	{
-		$user = factory(User::class)->create([ 'password' => bcrypt(12345) ]);
+		factory(User::class)->create([ 'password' => bcrypt(12345) ]);
 
 		$credentials = [
 			'password' => 12345,
@@ -38,7 +56,7 @@ class AuthenticationTest extends TestCase
 
 		$this->postJson('/api/login', $credentials)
 			->seeStatusCode(401)
-			->seeJson([	'error' => 'Unauthorized access' ]);
+			->seeJson([ 'error' => 'Unauthorized access' ]);
 	}
 
 	public function testEmptyPassword()
@@ -51,7 +69,7 @@ class AuthenticationTest extends TestCase
 
 		$this->postJson('/api/login', $credentials)
 			->seeStatusCode(401)
-			->seeJson([	'error' => 'Unauthorized access' ]);
+			->seeJson([ 'error' => 'Unauthorized access' ]);
 	}
 
 	public function testEmptyPayload()
@@ -60,7 +78,7 @@ class AuthenticationTest extends TestCase
 
 		$this->postJson('/api/login')
 			->seeStatusCode(401)
-			->seeJson([	'error' => 'Unauthorized access' ]);
+			->seeJson([ 'error' => 'Unauthorized access' ]);
 	}
 
 	public function testInvalidEmail()
@@ -74,7 +92,7 @@ class AuthenticationTest extends TestCase
 
 		$this->postJson('/api/login', $credentials)
 			->seeStatusCode(401)
-			->seeJson([	'error' => 'Unauthorized access' ]);
+			->seeJson([ 'error' => 'Unauthorized access' ]);
 	}
 
 	public function testInvalidPassword()
@@ -88,6 +106,6 @@ class AuthenticationTest extends TestCase
 
 		$this->postJson('/api/login', $credentials)
 			->seeStatusCode(401)
-			->seeJson([	'error' => 'Unauthorized access' ]);
+			->seeJson([ 'error' => 'Unauthorized access' ]);
 	}
 }
