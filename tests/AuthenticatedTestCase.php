af6865c08aaaaeb727335362ae7c60a1f01581ab
Wannes Gennar <wannes.gennar@gmail.com>
2016-05-01 22:20:27 +0200

    add null checks on authenticatedtest

--- tests/AuthenticatedTestCase.php
+++ tests/AuthenticatedTestCase.php
@@ -35,8 +35,14 @@ class AuthenticatedTestCase extends Illuminate\Foundation\Testing\TestCase
 		return $app;
 	}
 
+	/**
+	 * Authenticate a user for the unit test.
+	 */
 	public function authenticate()
 	{
+		if ( ! is_null($this->user) )
+			return;
+
 		$this->user = factory(User::class)->create([ 'password' => bcrypt(12345) ]);
 
 		$credentials = [
@@ -47,16 +53,22 @@ class AuthenticatedTestCase extends Illuminate\Foundation\Testing\TestCase
 		$this->postJson('/api/login', $credentials);
 	}
 
+	public function logout()
+	{
+		$this->user = null;
+	}
+
 	/**
 	 * Visit the given URI with a JSON GET request.
 	 *
-	 * @param  string  $uri
-	 * @param  array  $headers
+	 * @param  string $uri
+	 * @param  array $headers
 	 * @return $this
 	 */
-	public function getJson($uri, array $headers = [])
+	public function getJson($uri, array $headers = [ ])
 	{
-		$headers['Authorization'] = $this->user->secret;
+		if ( ! is_null($this->user) )
+			$headers[ 'Authorization' ] = $this->user->secret;
 
 		return parent::get($uri, $headers);
 	}
@@ -64,14 +76,15 @@ class AuthenticatedTestCase extends Illuminate\Foundation\Testing\TestCase
 	/**
 	 * Visit the given URI with a POST request.
 	 *
-	 * @param  string  $uri
-	 * @param  array  $data
-	 * @param  array  $headers
+	 * @param  string $uri
+	 * @param  array $data
+	 * @param  array $headers
 	 * @return $this
 	 */
 	public function postJson($uri, array $data = [ ], array $headers = [ ])
 	{
-		$headers['Authorization'] = $this->user->secret;
+		if ( ! is_null($this->user) )
+			$headers[ 'Authorization' ] = $this->user->secret;
 
 		return parent::post($uri, $data, $headers);
 	}
