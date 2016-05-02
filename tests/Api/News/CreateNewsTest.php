36508a5f7d5f6106e3514a6fd1cacd65192f07db
Wannes Gennar <wannes.gennar@gmail.com>
2016-05-02 10:04:35 +0200

    added unit tests for news item creation

--- tests/Api/News/CreateNewsTest.php
+++ tests/Api/News/CreateNewsTest.php
@@ -6,16 +6,80 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class CreateNewsTest extends AuthenticatedTestCase
 {
+	CONST TABLE_NAME = 'newsitems';
+
+	const VALID_NEWS = [
+		'title' => 'foo article',
+		'content' => 'lorem ipsum dolor amat!'
+	];
+
 	public function testValidCreation()
 	{
-		$news = [
-			'title' => 'foo article',
-			'content' => 'lorem ipsum dolor amat!'
-		];
+		$this->postJson('/api/news/create', CreateNewsTest::VALID_NEWS)
+			->seeStatusCode(200)
+			->seeJson(CreateNewsTest::VALID_NEWS)
+			->seeInDatabase(CreateNewsTest::TABLE_NAME, CreateNewsTest::VALID_NEWS);
+	}
+
+	public function testNoTitle()
+	{
+		$news = array_except(CreateNewsTest::VALID_NEWS, 'title');
 
 		$this->postJson('/api/news/create', $news)
-			->seeStatusCode(200)
-			->seeJson($news)// TODO see what JSON?
-			->seeInDatabase('newsitems', $news);
+			->seeStatusCode(422)
+			->seeJson([])
+			->dontSeeInDatabase(CreateNewsTest::TABLE_NAME, $news);
+	}
+
+	public function testEmptyTitle()
+	{
+		$news = CreateNewsTest::VALID_NEWS;
+
+		$news['title'] = '';
+
+		$this->postJson('/api/news/create', $news)
+			->seeStatusCode(422)
+			->seeJson([])
+			->dontSeeInDatabase(CreateNewsTest::TABLE_NAME, $news);
+	}
+
+	public function testNoContent()
+	{
+		$news = array_except(CreateNewsTest::VALID_NEWS, 'content');
+
+		$this->postJson('/api/news/create', $news)
+			->seeStatusCode(422)
+			->seeJson([])
+			->dontSeeInDatabase(CreateNewsTest::TABLE_NAME, $news);
+	}
+
+	public function testEmptyContent()
+	{
+		$news = CreateNewsTest::VALID_NEWS;
+
+		$news['content'] = '';
+
+		$this->postJson('/api/news/create', $news)
+			->seeStatusCode(422)
+			->seeJson([])
+			->dontSeeInDatabase(CreateNewsTest::TABLE_NAME, $news);
+	}
+
+	public function testEmptyPayload()
+	{
+		$this->postJson('/api/news/create')
+			->seeStatusCode(422)
+			->seeJson([])
+			->dontSeeInDatabase(CreateNewsTest::TABLE_NAME, []);
+	}
+
+	public function testNotAuthenticated()
+	{
+		$this->logout();
+
+		$this->postJson('/api/news/create', CreateNewsTest::VALID_NEWS)
+			->seeStatusCode(401)
+			->dontSeeJson(CreateNewsTest::VALID_NEWS)
+			->dontSeeInDatabase(CreateNewsTest::TABLE_NAME, CreateNewsTest::VALID_NEWS);
 	}
 }
