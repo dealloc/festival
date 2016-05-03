9d4a1361aca05a06067c11e8641d3f168b422043
Wannes Gennar <wannes.gennar@gmail.com>
2016-05-04 01:08:27 +0200

    test listing unauthorized

--- tests/Api/Tickets/RetrieveTicketsTest.php
+++ tests/Api/Tickets/RetrieveTicketsTest.php
@@ -8,7 +8,7 @@ class RetrieveTicketsTest extends AuthenticatedTestCase
 {
 	public function testValidListing()
 	{
-		factory(\Festival\Entities\Tickets\Ticket::class, 10);
+		factory(\Festival\Entities\Tickets\Ticket::class, 10)->create();
 
 		$this->getJson('/api/tickets')
 			->seeStatusCode(200)
@@ -17,5 +17,14 @@ class RetrieveTicketsTest extends AuthenticatedTestCase
 					'id', 'token', 'created_at'
 				]
 			]);
+	}
+
+	public function testUnauthorizedListing()
+	{
+		$this->logout();
+		factory(\Festival\Entities\Tickets\Ticket::class, 10)->create();
+
+		$this->getJson('/api/tickets')
+			->seeStatusCode(401);
 	}
 }
