e23a73be5e94299f1917ad8bc323cc2ba2752886
Wannes Gennar <wannes.gennar@gmail.com>
2016-05-03 02:34:12 +0200

    renamed newsitem_id to news_id in Comment

--- src/Entities/News/Comments/Comment.php
+++ src/Entities/News/Comments/Comment.php
@@ -1,11 +1,11 @@
 <?php
 // Created by dealloc. All rights reserved.
 
-namespace Festival\Entities\News\Comments;
+namespace Festival\Entities\Newsz\Comments;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-	protected $fillable = [ 'newsitem_id', 'content', 'user_id' ];
+	protected $fillable = [ 'news_id', 'content', 'user_id' ];
 }
