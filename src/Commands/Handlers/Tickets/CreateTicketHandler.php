392140fa3cbdd9a7b8cafa0c657a6fc06c2fa925
Wannes Gennar <wannes.gennar@gmail.com>
2016-05-04 01:19:27 +0200

    implemented CreateTicketHandler

--- src/Commands/Handlers/Tickets/CreateTicketHandler.php
+++ src/Commands/Handlers/Tickets/CreateTicketHandler.php
@@ -4,10 +4,31 @@
 namespace Festival\Commands\Handlers\Tickets;
 
 use Festival\Commands\Tickets\CreateTicketCommand;
+use Festival\Contracts\Auth\AuthenticateService;
+use Festival\Contracts\Repositories\Tickets\TicketRepository;
 
 class CreateTicketHandler
 {
+	/**
+	 * @var \Festival\Contracts\Repositories\Tickets\TicketRepository
+	 */
+	private $repository;
+	/**
+	 * @var \Festival\Contracts\Auth\AuthenticateService
+	 */
+	private $service;
+
+	public function __construct(TicketRepository $repository, AuthenticateService $service)
+	{
+		$this->repository = $repository;
+		$this->service = $service;
+	}
+
 	public function handle(CreateTicketCommand $command)
 	{
+		return $this->repository->create([
+			'user_id' => $this->service->user()->id,
+			'token' => uniqid('evento-', true)
+		]);
 	}
 }
