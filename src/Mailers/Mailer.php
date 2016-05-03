e5d96450f12ac9422fbfdcfb53c2a7f53e5075f0
Wannes Gennar <wannes.gennar@gmail.com>
2016-05-03 12:09:56 +0200

    added mailer

---
+++ src/Mailers/Mailer.php
@@ -0,0 +1,50 @@
+<?php
+// Created by dealloc. All rights reserved.
+
+namespace Festival\Mailers;
+
+use Illuminate\Contracts\Mail\Mailer as MailerContract;
+use Illuminate\Mail\Message;
+
+abstract class Mailer
+{
+	/**
+	 * @var \Illuminate\Contracts\Mail\Mailer
+	 */
+	private $mailer;
+	
+	public function __construct(MailerContract $mailer)
+	{
+		$this->mailer = $mailer;
+	}
+
+	/**
+	 * @param string $recipient
+	 * @param string $subject
+	 * @param string $view
+	 * @param array $data
+	 * @param array|null $sender
+	 */
+	public function send($recipient, $subject, $view, array $data = [], array $sender = null)
+	{
+		if ( is_null($sender) )
+			$sender = $this->getFrom();
+
+		$this->mailer->send($view, $data, function (Message $message) use ($recipient, $subject, $sender)
+		{
+			$message->to($recipient);
+			$message->subject($subject);
+			$message->from($sender);
+		});
+	}
+
+	/**
+	 * Get the senders address and name from the settings.
+	 *
+	 * @return array
+	 */
+	protected function getFrom()
+	{
+		return env('mail.from');
+	}
+}
