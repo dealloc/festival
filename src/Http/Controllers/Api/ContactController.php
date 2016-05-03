e0019f7bc27241298464a5c1242eef7e67c0ef8e
Wannes Gennar <wannes.gennar@gmail.com>
2016-05-03 10:54:24 +0200

    added validation for contact

--- src/Http/Controllers/Api/ContactController.php
+++ src/Http/Controllers/Api/ContactController.php
@@ -4,10 +4,11 @@
 namespace Festival\Http\Controllers\Api;
 
 use Festival\Http\Controllers\Controller;
+use Festival\Http\Requests\Contacts\CreateContactRequest;
 
 class ContactController extends Controller
 {
-	public function create()
+	public function create(CreateContactRequest $request)
 	{
 	}
 }
---
+++ src/Http/Requests/Contacts/CreateContactRequest.php
@@ -0,0 +1,21 @@
+<?php
+// Created by dealloc. All rights reserved.
+
+namespace Festival\Http\Requests\Contacts;
+
+use Festival\Http\Requests\Request;
+
+class CreateContactRequest extends Request
+{
+	/**
+	 * Define the rules for validating this request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		return [
+			'content' => 'required|string'
+		];
+	}
+}
