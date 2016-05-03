29215df149cef6b7d58fd844946c63b3a6b10d60
Wannes Gennar <wannes.gennar@gmail.com>
2016-05-03 17:41:14 +0200

    added user mailer

---
+++ src/Mailers/Users/UserMailer.php
@@ -0,0 +1,15 @@
+<?php
+// Created by dealloc. All rights reserved.
+
+namespace Festival\Mailers\Users;
+
+use Festival\Entities\Users\User;
+use Festival\Mailers\Mailer;
+
+class UserMailer extends Mailer
+{
+	public function welcome(User $user)
+	{
+		parent::send($user->email, 'Welcome to Evento!', 'emails.registration', compact('user'));
+	}
+}
