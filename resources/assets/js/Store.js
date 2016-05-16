426da005d7c7dc54641c13692bcfc4c92971a436
Wannes Gennar <wannes.gennar@gmail.com>
2016-05-17 01:40:06 +0200

    added vuex

---
+++ resources/assets/js/Store.js
@@ -0,0 +1,7 @@
+// Created by dealloc. All rights reserved.
+
+const state = {};
+
+const mutations = {};
+
+export default new Vuex.Store( { state, mutations } );
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,10 +1,12 @@
 // Created by dealloc. All rights reserved.
 
 let App = require('vue/App.vue');
+let Store = require('Store');
 
 new Vue({
 	el: 'body',
 	components: {
 		app: App
-	}
+	},
+	store: Store
 });
