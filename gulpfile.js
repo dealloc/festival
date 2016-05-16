d25d8170960f5ca5f3c4c98e54e1bbce1830f617
Wannes Gennar <wannes.gennar@gmail.com>
2016-05-17 01:08:50 +0200

    updated gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,10 @@
-var elixir = require('laravel-elixir');
+var gulp = require( 'gulp' );
+var webpack = require( 'webpack-stream' );
+var webpack_config = require( './webpack.config.js' );
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
-elixir(function(mix) {
-    mix.sass('app.scss');
-});
+gulp.task( 'default', function ()
+{
+	return gulp.src( 'src/entry.js' )
+		.pipe( webpack( webpack_config ) )
+		.pipe( gulp.dest( 'dist/' ) );
+} );
