f99d92d2949747d7a1a342f3bdd8315fbc4f83f2
Wannes Gennar <wannes.gennar@gmail.com>
2016-05-17 13:37:11 +0200

    fine tuned Gulp stack

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,23 @@ var gulp = require( 'gulp' );
 var webpack = require( 'webpack-stream' );
 var webpack_config = require( './webpack.config.js' );
 
-gulp.task( 'default', function ()
+gulp.task( 'webpack', function ()
 {
 	return gulp.src( 'src/entry.js' )
 		.pipe( webpack( webpack_config ) )
 		.pipe( gulp.dest( webpack_config.output.path ) );
 } );
+
+gulp.task( 'build', [ 'webpack' ] );
+
+gulp.task( 'watch', [ 'build' ], function()
+{
+	let watch_config = webpack_config;
+	watch_config.watch = true;
+
+	return gulp.src( 'src/entry.js' )
+		.pipe( webpack( watch_config ) )
+		.pipe( gulp.dest( webpack_config.output.path ) );
+});
+
+gulp.task( 'default', [ 'build' ] );
