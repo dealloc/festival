f76a27ff0eb88f50f55e5a8e6ba9ad0f15d02b01
Wannes Gennar <wannes.gennar@gmail.com>
2016-05-21 21:15:02 +0200

    updated gulp runtime

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,12 @@ var gulp = require( 'gulp' );
 var webpack = require( 'webpack-stream' );
 var webpack_config = require( './webpack.config.js' );
 
+if ( (process.argv.indexOf( '--dev' ) !== -1) )
+{
+	process.env.NODE_ENV = 'dev';
+	console.log('[GULP]: running in development mode!');
+}
+
 gulp.task( 'webpack', function ()
 {
 	return gulp.src( 'src/entry.js' )
@@ -9,9 +15,7 @@ gulp.task( 'webpack', function ()
 		.pipe( gulp.dest( webpack_config.output.path ) );
 } );
 
-gulp.task( 'build', [ 'webpack' ] );
-
-gulp.task( 'watch', [ 'build' ], function()
+gulp.task( 'webpack-watch', [ 'build' ], function ()
 {
 	let watch_config = webpack_config;
 	watch_config.watch = true;
@@ -19,6 +23,14 @@ gulp.task( 'watch', [ 'build' ], function()
 	return gulp.src( 'src/entry.js' )
 		.pipe( webpack( watch_config ) )
 		.pipe( gulp.dest( webpack_config.output.path ) );
-});
+} );
+
+gulp.task( 'semantic', require( './node_modules/semantic-ui/tasks/build' ) );
+
+gulp.task( 'semantic-watch', [ 'build' ], require( './node_modules/semantic-ui/tasks/watch' ) );
+
+gulp.task( 'build', [ 'webpack', 'semantic' ] );
+
+gulp.task( 'watch', [ 'webpack-watch', 'semantic-watch' ] );
 
 gulp.task( 'default', [ 'build' ] );
