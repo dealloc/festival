4eabdfe94e8c207ab7c38ad64e1efe7fba3277f3
Wannes Gennar <wannes.gennar@gmail.com>
2016-05-22 15:40:29 +0200

    perf(): load CSS async
    Explicitly declare a <body> and move CSS into it to load async

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
-<link rel="stylesheet" href="{{ asset('/vendor/semantic/dist/semantic.min.css') }}">
-<div id="root"></div>
-<script type="text/javascript" src="{{ asset('/vendor/jquery/dist/jquery.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('/vendor/semantic/dist/semantic.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('/js/app.min.js') }}" async></script>
+<body>
+	<div id="root"></div>
+	<link rel="stylesheet" href="{{ asset('/vendor/semantic/dist/semantic.min.css') }}" property="stylesheet">
+	<script type="text/javascript" src="{{ asset('/vendor/jquery/dist/jquery.min.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('/vendor/semantic/dist/semantic.min.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('/js/app.min.js') }}" async></script>
+</body>
