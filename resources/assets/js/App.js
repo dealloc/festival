8336e8cfce47f33e2a543caf601a2671771f8fd7
Wannes Gennar <wannes.gennar@gmail.com>
2016-05-23 17:20:08 +0200

    feat: load store in App
    Load the Vuex store into the App component

--- resources/assets/js/App.js
+++ resources/assets/js/App.js
@@ -1,9 +1,11 @@
 // Created by dealloc. All rights reserved.
 
 import SideMenu from 'components/Menu.vue';
+import Store from 'Store';
 
 export default {
 	components: {
 		SideMenu
-	}
+	},
+	Store
 }
