6d472045aa41724334f2d5eec86c339292731378
Wannes Gennar <wannes.gennar@gmail.com>
2016-05-23 16:01:33 +0200

    feat(browser): setup caching
    Setup HTML5 caching in the Gulp pipeline (disables in dev)

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
-var gulp = require( 'gulp' );
-var webpack = require( 'webpack-stream' );
-var webpack_config = require( './webpack.config.js' );
+var gulp = require('gulp');
+var manifest = require('gulp-manifest');
+var webpack = require('webpack-stream');
+var webpack_config = require('./webpack.config.js');
 
-if ( (process.argv.indexOf( '--dev' ) !== -1) )
+if ((process.argv.indexOf('--dev') !== -1))
 {
 	console.log('[GULP]: running in development mode!');
 }
 
-gulp.task('vendor', function()
+gulp.task('manifest', function ()
+{
+	let sources = ((process.argv.indexOf('--dev') !== -1) ? '' : 'public/**/+(*.min.*|*.woff2)');
+	gulp.src(sources)
+		.pipe(manifest({
+			hash: true,
+			filename: 'application.manifest'
+		}))
+		.pipe(gulp.dest('public/'));
+});
+
+gulp.task('vendor', function ()
 {
 	gulp.src('node_modules/jquery/dist/jquery.min.js')
 		.pipe(gulp.dest('public/vendor/jquery/dist'));
@@ -15,26 +27,26 @@ gulp.task('vendor', function()
 		.pipe(gulp.dest('public/vendor/semantic/dist'));
 });
 
-gulp.task( 'webpack', function ()
+gulp.task('webpack', function ()
 {
-	return gulp.src( 'src/entry.js' )
-		.pipe( webpack( webpack_config ) )
-		.pipe( gulp.dest( webpack_config.output.path ) );
-} );
+	return gulp.src('src/entry.js')
+		.pipe(webpack(webpack_config))
+		.pipe(gulp.dest(webpack_config.output.path));
+});
 
-gulp.task( 'webpack-watch', [ 'build' ], function ()
+gulp.task('webpack-watch', ['build'], function ()
 {
 	let watch_config = webpack_config;
 	watch_config.watch = true;
 
-	return gulp.src( 'src/entry.js' )
-		.pipe( webpack( watch_config ) )
-		.pipe( gulp.dest( webpack_config.output.path ) );
-} );
+	return gulp.src('src/entry.js')
+		.pipe(webpack(watch_config))
+		.pipe(gulp.dest(webpack_config.output.path));
+});
 
-gulp.task( 'semantic', require( './node_modules/semantic-ui/tasks/build/css' ) );
+gulp.task('semantic', require('./node_modules/semantic-ui/tasks/build/css'));
 gulp.task('semantic-assets', require('./node_modules/semantic-ui/tasks/build/assets'));
-gulp.task( 'semantic-watch', [ 'build' ], require( './node_modules/semantic-ui/tasks/watch' ) );
-gulp.task( 'build', [ 'webpack', 'semantic', 'semantic-assets', 'vendor' ] );
-gulp.task( 'watch', [ 'webpack-watch', 'semantic-watch' ] );
-gulp.task( 'default', [ 'build' ] );
+gulp.task('semantic-watch', ['build'], require('./node_modules/semantic-ui/tasks/watch'));
+gulp.task('build', ['webpack', 'semantic', 'semantic-assets', 'vendor', 'manifest']);
+gulp.task('watch', ['webpack-watch', 'semantic-watch']);
+gulp.task('default', ['build']);
