8a52e3ef1a44068b0a4235a93ada0d6428357d67
Wannes Gennar <wannes.gennar@gmail.com>
2016-05-23 21:52:21 +0200

    feat(browser): load newsitems
    Load all news items and show them in cards on the homepage

--- resources/assets/vue/pages/Home.vue
+++ resources/assets/vue/pages/Home.vue
@@ -1,7 +1,52 @@
 <template>
-	<h1>Welcome to the homepage!</h1>
+	<div class="ui container">
+		<div class="ui special stackable cards">
+			<div class="card" v-for="card in cards" track-by="identifier">
+				<div class="blurring dimmable image">
+					<div class="ui dimmer">
+						<div class="content">
+							<div class="center">
+								<i class="big icon wait"></i> 11PM saturday
+							</div>
+						</div>
+					</div>
+					<img src="http://semantic-ui.com/images/avatar/large/elliot.jpg">
+				</div>
+				<div class="content">
+					<a class="header">{{ card.title }}</a>
+					<div class="meta">
+						<span class="date">{{ card.created_at }}</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-	export default { name: 'Home' }
+	export default {
+		name: 'Home',
+		created() {
+			$.get('/api/news').then(function (res) {
+				this.cards = res.data;
+			}.bind(this));
+		},
+		data() {
+			return { cards: [] }
+		}
+	}
 </script>
+
+<style lang="sass" scoped>
+	.ui.container
+	{
+		margin-top: 1vh;
+	}
+	@media only screen and (max-width: 767px)
+	{
+		.ui.container
+		{
+			margin-top: 3vh;
+		}
+	}
+</style>
