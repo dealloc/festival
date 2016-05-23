d527a52564c469bc93e088b0e7b537530f0e72a7
Wannes Gennar <wannes.gennar@gmail.com>
2016-05-23 17:47:55 +0200

    feat(browser): close menu on click
    Close the sidemenu when you click on an item

--- resources/assets/vue/ui/Menu.vue
+++ resources/assets/vue/ui/Menu.vue
@@ -1,19 +1,19 @@
 <template>
 	<div class="ui sidebar inverted vertical menu">
-		<a class="item" v-link="{ name: 'home' }">
-			1
-		</a>
-		<a class="item" v-link="{ name: 'home' }">
-			2
-		</a>
-		<a class="item" v-link="{ name: 'home' }">
-			3
+		<a @click="close()" class="item" v-link="{ name: 'home' }">
+			Home
 		</a>
 	</div>
 </template>
 
 <script>
-	export default {}
+	export default {
+		methods: {
+			close() {
+				$(this.$el).sidebar('hide');
+			}
+		}
+	}
 </script>
 
 <style lang="sass" scoped>
