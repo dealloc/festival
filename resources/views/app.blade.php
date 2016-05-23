4a993d92b0b7b172a13a9a554d6e878eb8b4b4d0
Wannes Gennar <wannes.gennar@gmail.com>
2016-05-23 15:40:44 +0200

    feat(mobile): made app progressive
    Made app progressive by providing additional meta data and a manifest file for more "native" look on
    mobile

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,21 +1,30 @@
 <!DOCTYPE html>
-<meta charset="utf-8" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-<body>
-	<side-menu></side-menu>
-	<div class="pusher">
-		<div class="ui top attached demo menu">
-			<a class="item" data-menu-toggle>
-				<i class="sidebar icon"></i>
-				Menu
-			</a>
-		</div>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="author" content="Wannes Gennar">
+		<link rel="manifest" href="{{ asset('/manifest.json') }}">
+		<meta name="theme-color" content="#A90D16">
+		<link rel="icon" sizes="192x192" href="{{ asset('/icons/logo-192.png') }}">
+		<title>EHB rock</title>
+	</head>
+	<body>
+		<side-menu></side-menu>
+		<div class="pusher">
+			<div class="ui top attached demo menu">
+				<a class="item" data-menu-toggle>
+					<i class="sidebar icon"></i>
+					Menu
+				</a>
+			</div>
 
-		<router-view></router-view>
-	</div>
-	<link rel="stylesheet" href="{{ asset('/vendor/semantic/dist/semantic.min.css') }}" property="stylesheet">
-	<script type="text/javascript" src="{{ asset('/vendor/jquery/dist/jquery.min.js') }}"></script>
-	<script type="text/javascript" src="{{ asset('/vendor/semantic/dist/semantic.min.js') }}"></script>
-	<script type="text/javascript" src="{{ asset('/js/app.min.js') }}" async></script>
-</body>
+			<router-view></router-view>
+		</div>
+		<link rel="stylesheet" href="{{ asset('/vendor/semantic/dist/semantic.min.css') }}" property="stylesheet">
+		<script type="text/javascript" src="{{ asset('/vendor/jquery/dist/jquery.min.js') }}"></script>
+		<script type="text/javascript" src="{{ asset('/vendor/semantic/dist/semantic.min.js') }}"></script>
+		<script type="text/javascript" src="{{ asset('/js/app.min.js') }}" async></script>
+	</body>
+</html>
