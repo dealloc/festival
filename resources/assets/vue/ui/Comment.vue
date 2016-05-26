f575b25a74ee317586e75e2364c78d884af745cb
Wannes Gennar <wannes.gennar@gmail.com>
2016-05-26 03:18:07 +0200

    feat(vue, browser): implemented comments
    Implemented the comment component

--- resources/assets/vue/ui/Comment.vue
+++ resources/assets/vue/ui/Comment.vue
@@ -4,21 +4,48 @@
 			<img src="http://semantic-ui.com/images/avatar/small/stevie.jpg">
 		</a>
 		<div class="content">
-			<a class="author">Stevie Feliciano</a>
+			<a class="author">{{ author }}</a>
 			<div class="metadata">
-				<div class="date">2 days ago</div>
+				<div class="date">{{ written }}</div>
 			</div>
 			<div class="text">
-				Hey guys, I hope this example comment is helping you read this documentation.
+				{{ content }}
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
+	import moment from 'moment';
+
 	export default {
 		name: 'comment',
-		props: ['comment']
+		props: ['comment'],
+		data() {
+			return { timer: null, written: '' }
+		},
+		computed: {
+			author() {
+				if (this.comment === (void 0) || this.comment.author === (void 0)) return '';
+				return `${this.comment.author.fname} ${this.comment.author.lname}`;
+			},
+			content() {
+				if (this.comment === (void 0) || this.comment.content === (void 0)) return '';
+				return this.comment.content;
+			}
+		},
+		methods: {
+			update() {
+				this.written = moment(this.comment.created_at).fromNow();
+			}
+		},
+		ready() {
+			this.update();
+			this.timer = setInterval(this.update.bind(this), 15000);
+		},
+		beforeDestroy() {
+			clearInterval(this.timer);
+		}
 	}
 </script>
 
