28d7436f94874fed59576449ac813fb31a110b4a
Wannes Gennar <wannes.gennar@gmail.com>
2016-05-26 02:48:12 +0200

    feat(api, debug): added comment factory
    Added factory to seed comments

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,32 +11,37 @@
 |
 */
 
-$factory->define(Festival\Entities\Users\User::class, function (Faker\Generator $faker)
-{
-	return [
-		'fname'          => $faker->firstName,
-		'lname'          => $faker->lastName,
-		'email'          => $faker->safeEmail,
-		'password'       => bcrypt(str_random(10)),
-		'secret'         => md5(uniqid()),
-		'remember_token' => str_random(10),
-	];
+$factory->define(Festival\Entities\Users\User::class, function (Faker\Generator $faker) {
+    return [
+        'fname'          => $faker->firstName,
+        'lname'          => $faker->lastName,
+        'email'          => $faker->safeEmail,
+        'password'       => bcrypt(str_random(10)),
+        'secret'         => md5(uniqid()),
+        'remember_token' => str_random(10),
+    ];
 });
 
-$factory->define(\Festival\Entities\News\News::class, function (Faker\Generator $faker)
-{
-	return [
-		'identifier' => md5(uniqid()),
-		'title'   => $faker->text,
-		'content' => $faker->realText(),
-		'user_id' => function() { return factory(\Festival\Entities\Users\User::class)->create()->id; }
-	];
+$factory->define(\Festival\Entities\News\News::class, function (Faker\Generator $faker) {
+    return [
+        'identifier' => md5(uniqid()),
+        'title'      => $faker->text(15),
+        'content'    => $faker->realText(),
+        'user_id'    => function () { return factory(\Festival\Entities\Users\User::class)->create()->id; },
+    ];
 });
 
-$factory->define(\Festival\Entities\Tickets\Ticket::class, function (Faker\Generator $faker)
-{
-	return [
-		'user_id' => function() { return factory(\Festival\Entities\Users\User::class)->create()->id; },
-		'token' => uniqid('evento-', true),
-	];
+$factory->define(\Festival\Entities\News\Comments\Comment::class, function (Faker\Generator $faker) {
+    return [
+        'user_id' => function () { return factory(\Festival\Entities\Users\User::class)->create()->id; },
+        'news_id' => function () { return factory(\Festival\Entities\News\News::class)->create()->id; },
+        'content' => $faker->realText(),
+    ];
+});
+
+$factory->define(\Festival\Entities\Tickets\Ticket::class, function (Faker\Generator $faker) {
+    return [
+        'user_id' => function () { return factory(\Festival\Entities\Users\User::class)->create()->id; },
+        'token'   => uniqid('evento-', true),
+    ];
 });
