eb841cca964d71c03f2586637f63fc3e957f5b88
Wannes Gennar <wannes.gennar@gmail.com>
2016-05-28 21:40:47 +0200

    feat(api): validate update user
    Add a route to validate user information

--- src/Http/Controllers/Api/UserController.php
+++ src/Http/Controllers/Api/UserController.php
@@ -5,11 +5,17 @@ namespace Festival\Http\Controllers\Api;
 
 use Festival\Contracts\Auth\AuthenticateService;
 use Festival\Http\Controllers\Controller;
+use Festival\Http\Requests\Users\UpdateUserRequest;
 
 class UserController extends Controller
 {
 	public function get(AuthenticateService $service)
 	{
 		return $service->user();
+	}
+
+	public function update(UpdateUserRequest $request)
+	{
+		return [];
 	}
 }
