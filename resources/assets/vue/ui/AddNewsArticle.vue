c280133dc76fc3a5341617cac8b5e6fc544b42f5
Wannes Gennar <wannes.gennar@gmail.com>
2016-05-29 13:53:27 +0200

    feat(vue): implemented add news article
    Implemented the add news article component

--- resources/assets/vue/ui/AddNewsArticle.vue
+++ resources/assets/vue/ui/AddNewsArticle.vue
@@ -1,18 +1,73 @@
 <template>
 	<div class="modals">
 		<div class="ui modal" v-el:form>
-			<div class="header">Create new news article</div>
 			<div class="content">
-				<p>form goes here</p>
+				<div class="ui form">
+					<h4 class="ui dividing header">Create new news article</h4>
+					<div class="field">
+						<input v-model="title" type="text" placeholder="Article title">
+					</div>
+					<div class="field" v-if="!preview">
+						<textarea v-model="article" placeholder="Article content"></textarea>
+					</div>
+					<div class="ui segment" v-if="preview">
+						{{{ article | markdown }}}
+					</div>
+				</div>
+			</div>
+			<div class="actions">
+				<div :class="{ 'loading': loading }" v-if="!loading" class="ui cancel negative button">Cancel</div>
+				<div :class="{ 'loading': loading }" class="ui blue button" @click="togglePreview()">Preview</div>
+				<div :class="{ 'loading': loading }" class="ui green button" @click="submit()">Submit</div>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
+	import 'filters/markdown';
+
 	export default {
+		data() {
+			return {
+				preview: false,
+				title: '',
+				article: '',
+				loading: false
+			}
+		},
+		ready() {
+		},
+		methods: {
+			togglePreview() {
+				this.preview = !this.preview;
+			},
+			submit() {
+				if (this.loading) return;
+
+				this.loading = true;
+				$.post('/api/news', { title: this.title, content: this.article })
+				.then(this.submitted.bind(this))
+				.fail(this.failed.bind(this))
+				.always(r => $(this.$els.form).modal('hide'));
+			},
+			submitted() {
+				this.$dispatch('toast', { message: `Article created!` });
+			},
+			failed() {
+				this.$dispatch('toast', { message: `Failed to post article! (did you miss a field?)` });
+			}
+		},
+		events: {
+			'add-news-article::show'() {
+				$(this.$els.form).modal('show');
+			}
+		}
 	}
 </script>
 
 <style lang="sass" scoped>
+	.content {
+		text-align: center;
+	}
 </style>
