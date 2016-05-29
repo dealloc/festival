462939f0650935bd273fe90d7be5d054c5ba74e9
Wannes Gennar <wannes.gennar@gmail.com>
2016-05-29 17:41:16 +0200

    feat(browser, vue): load artists
    Load the artists in the lineup page

--- resources/assets/vue/pages/Lineup.vue
+++ resources/assets/vue/pages/Lineup.vue
@@ -1,10 +1,43 @@
 <template>
-	<h1>lineup</h1>
+	<div class="ui container">
+		<div class="ui link cards">
+			<div class="card" v-for="artist in artists">
+				<div class="image">
+					<img :src="artist.image">
+				</div>
+				<div class="content">
+					<div class="header">{{ artist.name }}</div>
+					<div class="meta">
+						<a>{{ artist.start }} - {{ artist.end }}</a>
+					</div>
+					<div class="description">
+						{{ artist.description }}
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
+	import moment from 'moment';
+
 	export default {
-		name: 'lineup'
+		name: 'lineup',
+		data() {
+			return {
+				artists: []
+			};
+		},
+		created() {
+			$.get('/api/lineup').then(r => {
+				this.artists = r.data;
+				this.artists.forEach((e) => {
+					e.start = moment(e.start).fromNow();
+					e.end = moment(e.end).fromNow();
+				});
+			});
+		}
 	}
 </script>
 
