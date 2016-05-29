6484b441e23d21bf89cec505c769791cb52190d8
Wannes Gennar <wannes.gennar@gmail.com>
2016-05-29 12:23:40 +0200

    feat(vue): create add news article component
    Created a component to create new news articles

---
+++ resources/assets/vue/ui/AddNewsArticle.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="modals">
+		<div class="ui modal" v-el:form>
+			<div class="header">Create new news article</div>
+			<div class="content">
+				<p>form goes here</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+	}
+</script>
+
+<style lang="sass" scoped>
+</style>
--- resources/assets/vue/ui/index.js
+++ resources/assets/vue/ui/index.js
@@ -3,9 +3,11 @@
 import NewsItem from 'ui/NewsItem.vue';
 import SideMenu from 'ui/SideMenu.vue';
 import Comment from 'ui/Comment.vue';
+import AddNewsArticle from 'ui/AddNewsArticle.vue';
 
 export {
 	NewsItem,
 	SideMenu,
-	Comment
+	Comment,
+	AddNewsArticle
 }
