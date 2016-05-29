ce3c3975efc492e1a8c03e32f64ea55ca84d41aa
Wannes Gennar <wannes.gennar@gmail.com>
2016-05-29 11:47:33 +0200

    feat(vue): implemented registration
    Implemented the registration logic and the validation

--- resources/assets/vue/pages/Registration.vue
+++ resources/assets/vue/pages/Registration.vue
@@ -7,41 +7,49 @@
 					Register your account
 				</div>
 			</h2>
-			<section class="ui large form" :class="{ 'loading': loading }">
+			<section class="ui large form" :class="{ 'loading': loading, 'error': error }">
 				<div class="ui stacked segment">
 					<div class="field">
 						<div class="ui left icon input">
 							<i class="user icon"></i>
-							<input type="text" name="fname" placeholder="First name">
+							<input v-model="fname" type="text" name="fname" placeholder="First name">
 						</div>
 					</div>
 					<div class="field">
 						<div class="ui left icon input">
 							<i class="user icon"></i>
-							<input type="text" name="lname" placeholder="Last name">
+							<input v-model="lname" type="text" name="lname" placeholder="Last name">
 						</div>
 					</div>
 					<div class="field">
 						<div class="ui left icon input">
 							<i class="mail icon"></i>
-							<input type="email" name="email" placeholder="E-mail address">
+							<input v-model="email" type="email" name="email" placeholder="E-mail address">
 						</div>
 					</div>
 					<div class="field">
 						<div class="ui left icon input">
 							<i class="lock icon"></i>
-							<input type="password" name="password" placeholder="Password">
+							<input v-model="password" type="password" name="password" placeholder="Password">
 						</div>
 					</div>
 					<div class="field">
 						<div class="ui left icon input">
 							<i class="lock icon"></i>
-							<input type="password" name="password_confirmation" placeholder="Password confirmation">
+							<input v-model="password_confirm" type="password" name="password_confirmation" placeholder="Password confirmation">
 						</div>
 					</div>
-					<button class="ui fluid large red button">register</button>
+					<button class="ui fluid large red button" @click="register()">register</button>
 				</div>
 
+				<div class="ui error message">
+					<div class="header">We couldn't register you</div>
+					<ul class="list">
+						<li v-for="message in error">
+							{{ message }}
+						</li>
+					</ul>
+				</div>
 			</section>
 
 			<div class="ui message">
@@ -55,7 +63,38 @@
 	export default {
 		name: 'registration',
 		data() {
-			return { loading: false }
+			return {
+				loading: false,
+				error: false,
+				fname: '',
+				lname: '',
+				email: '',
+				password: '',
+				password_confirm: ''
+			}
+		},
+		methods: {
+			register() {
+				this.loading = true;
+				$.post('/api/register', {
+					email   : this.email,
+					fname   : this.fname,
+					lname   : this.lname,
+					password: this.password,
+					password_confirmation: this.password_confirm
+				})
+				.then(this.registered.bind(this))
+				.fail(this.failed.bind(this))
+				.always(r => this.loading = false);
+			},
+			registered() {
+				this.$dispatch('toast', { message: `We registered your account!` });
+				this.$router.go({ name: 'login' });
+			},
+			failed(res) {
+				this.error = res.responseJSON.reason;
+				setTimeout(() => this.error = null, 3000);
+			}
 		}
 	}
 </script>
